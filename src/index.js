68929157b5280f2465e2c3bc2fea6b5c2b6a15da
Ochiengsteven <stevenochieng6305@gmail.com>
2023-06-23 15:03:05 +0300

    add display scores function

--- src/index.js
+++ src/index.js
@@ -28,4 +28,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const data = await response.json();
     form.reset();
   };
+
+  const display = (scores) => {
+    const scoresTable = document.getElementById('score-table');
+    scoresTable.innerHTML = '';
+    const table = document.createElement('table');
+    table.classList.add('table');
+
+    const tbody = document.createElement('tbody');
+
+    scores.forEach((item, index) => {
+      const row = document.createElement('tr');
+      row.classList.add(index % 2 === 0 ? 'even' : 'odd');
+      const cell = document.createElement('td');
+      cell.textContent = `${item.user}: ${item.score}`;
+      row.appendChild(cell);
+      tbody.appendChild(row);
+    });
+
+    table.appendChild(tbody);
+    scoresTable.appendChild(table);
+  };
 });
